4167647f4dba9d207c65c1c464da6182ae786bcf
IkeyBenz <ikey.benz@gmail.com>
2020-04-22 16:50:09 -0700

    [cleanup] renamed params to be consistent with leet codes

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -22,12 +22,12 @@ class ListNode:
 
 
 class Solution:
-    def mergeTwoLists(self, a, b):
-        if a and b:
-            if a.val > b.val:
-                a, b = b, a
-            a.next = self.mergeTwoLists(a.next, b)
-        return a or b
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1 or l2
 
 
 def make_linked(lst: list) -> ListNode:
