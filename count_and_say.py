33271187df03d2fc5395d5c92a751d73b79a9070
IkeyBenz <ikey.benz@gmail.com>
2020-04-22 17:31:26 -0700

    documentation for count_and_say problem

--- count_and_say.py
+++ count_and_say.py
@@ -1,3 +1,16 @@
+'''
+https://leetcode.com/problems/count-and-say/
+
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+    1.     1
+    2.     11
+    3.     21
+    4.     1211
+    5.     111221
+
+'''
+
+
 class Solution:
     def split_by_change(self, s: str) -> [str]:
         splits = [s[0]]
@@ -10,7 +23,7 @@ class Solution:
 
     def countAndSay(self, n: int) -> str:
         last = '1'
-        for _ in range(n):
+        for _ in range(n-1):
             splits = self.split_by_change(last)
             new = ''
             for split in splits:
@@ -21,4 +34,8 @@ class Solution:
 
 if __name__ == '__main__':
     s = Solution()
+    print(s.countAndSay(1))  # 1
+    print(s.countAndSay(2))  # 11
+    print(s.countAndSay(3))  # 21
+    print(s.countAndSay(4))  # 1211
     print(s.countAndSay(5))  # 111221
